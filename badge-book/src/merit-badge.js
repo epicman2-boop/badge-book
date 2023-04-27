900af877d452301000e91bcc2dbb38828c3a1c4c
Joseph Converse <jpc6459@psu.edu>
2023-04-27 12:30:21 -0400

    v4

--- badge-book/src/merit-badge.js
+++ badge-book/src/merit-badge.js
@@ -1,83 +1,95 @@
 import { LitElement, html, css } from "lit";
-import "../src/circle-wrap.js";
+import "@lrnwebcomponents/absolute-position-behavior/absolute-position-behavior.js";
 import "@lrnwebcomponents/simple-icon/simple-icon.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icon-button.js";
+import "../src/circle-wrap.js";
+import "../src/badge-sticker.js";
+import "../src/locked-badge.js";
 
-class BadgeSticker extends LitElement {
+class MeritBadge extends LitElement {
   static properties = {
+    date: { type: String },
     logo: { type: String },
     title: { type: String },
-    date: { type: String },
+    detailsIcon: { type: String },
     verificationLink: { type: String },
+    skills: { type: String },
+    criteriaName: { type: String },
+    skillsOpened: { type: Boolean },
   };
 
   static styles = css`
-    .circle {
+    #badge {
       position: relative;
-      padding: 20px;
-      margin: 20px;
-      width: 200px;
-      height: 200px;
-      background: #ff0030;
-      color: #fff;
-      font-size: 21px;
-      font-weight: bold;
-      line-height: 1.3em;
-      border: 2px dashed #fff;
-      border-radius: 50%;
-      box-shadow: 0 0 0 4px #ff0030, 2px 1px 6px 4px rgba(10, 10, 0, 0.5);
-      text-shadow: -1px -1px #aa3030;
-      font-weight: normal;
+      z-index: 2;
     }
-    circle-wrap {
+    .verification {
       position: absolute;
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
     }
-    .logo {
-      --simple-icon-width: 75px;
-      --simple-icon-height: 75px;
+    .skillsOpen { 
       position: absolute;
-      top: 50%;
       left: 50%;
-      transform: translate(-50%, -50%);
-    }
-    .bottom-links {
-      position: absolute;
-      top: 92%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      text-decoration: none;
+      z-index: 3;
     }
   `;
 
   constructor() {
     super();
-    this.logo = "simple-icon:check";
-    this.title = "Badge Title";
-    this.date = "2021-01-01";
-    this.verificationLink = "https://www.example.com";
+    this.skills = [];
+    this.skillsOpened = false;
   }
 
   render() {
     return html`
-      <div class="circle">
-        <circle-wrap title="${this.title}" date="${this.date}"></circle-wrap>
-        <simple-icon
-          class="logo"
-          icon="${this.logo}"
-          accent-color="purple"
-        ></simple-icon>
-        <div class="bottom-links">
-          <simple-icon-button icon="editor:insert-link"></simple-icon-button>
-          <simple-icon-button icon="star"></simple-icon-button>
-          <simple-icon-button icon="description"></simple-icon-button>
-        </div>
+      <locked-badge ?hidden="${this.skillsOpened}"></locked-badge>
+      <div id="badge">
+        <badge-sticker
+          id="badge-sticker"
+          logo="${this.logo}"
+          title="${this.title}"
+          date="${this.date}"
+          verificationLink="${this.verificationLink}"
+          ?hidden="${!this.skillsOpened}"
+        ></badge-sticker>
+        <simple-icon-button
+          class="skillsOpen"
+          icon="cancel"
+          @click="${this.skillClick}"
+        ></simple-icon-button>
       </div>
+      <absolute-position-behavior
+        justify
+        position="bottom"
+        allow-overlap
+        sticky
+        auto
+        .target="${this.activeNode}"
+        ?hidden="${!this.skillsOpened}"
+      >
+        ${this.skills.map(
+          (item) => html` <ol>
+            <li>${item}</li>
+          </ol>`
+        )}
+      </absolute-position-behavior>
     `;
+  }
+
+  firstUpdated(changedProperties) {
+    if (super.firstUpdated) {
+      super.firstUpdated(changedProperties);
+    }
+    this.activeNode = this.shadowRoot.querySelector("#badge");
+  }
+
+  skillClick(e) {
+    this.skillsOpened = !this.skillsOpened;
+    console.log("skillClick", this.skillsOpened);
   }
 }
 
-customElements.define("badge-sticker", BadgeSticker);
+customElements.define("merit-badge", MeritBadge);
